6789eca2bef55b0cd559a9847178b0f0bf7ea54f
Ethan Evans <graphicshark@gmail.com>
2017-01-21 14:29:39 -0700

    JS cleanup
    Cleaned up some of the JS for easier editing

--- js/SmoothScrolling.js
+++ js/SmoothScrolling.js
@@ -1,39 +1,48 @@
+// Smoothly Scrolls down to desired link
 $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 800);
-        return false;
-      }
-    }
-  });
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 800);
+				return false;
+			}
+		}
+	});
 });
 
+
+// Color change with page scroll
 $(document).scroll(function () {
-  var y = $(this).scrollTop();
-  if (y > 150) {
-    $('.navbar').css({ 'background-color': '#0F4556' })
-    $('.triangle').css({ 'border-color': '#0F4556 transparent transparent transparent' })
-    $('.nav-links').css({ 'top': '-5px' })
-    $('.logo').attr( 'src', 'img/navbar-Logo-I.svg' )
-  } else {
-    $('.logo').attr( 'src', 'img/navbar-Logo.svg' )
-    $('.navbar').css({ 'background-color': '#D2DBBD' })
-    $('.triangle').css({ 'border-color': '#D2DBBD transparent transparent transparent' })
-    $('.nav-links').css({ 'top': '60px' })
-  }
-  var docWidth = $( document ).width();
-  if(docWidth < 1280){
-    if (y > 625) {
-    $('.navbar').css({ 'background-color': '#0F4556'})
-    $('.triangle').css({ 'border-color': '#0F4556 transparent transparent transparent' })
-  } else {
-    $('.navbar').css({ 'background-color': '#D2DBBD' })
-    $('.triangle').css({ 'border-color': '#D2DBBD transparent transparent transparent' })
-  }
+	var y = $(this).scrollTop();
+	var docWidth = $( document ).width();
+	var blue = '#0F4556';
+	var blueTri = '#0F4556 transparent transparent transparent';
+	var green = '#D2DBBD';
+	var greenTri = '#D2DBBD transparent transparent transparent';
+
+	if (y > 150) {
+		$('.navbar').css({ 'background-color': blue })
+		$('.triangle').css({ 'border-color': blueTri })
+		$('.nav-links').css({ 'top': '-5px' })
+		$('.logo').attr( 'src', 'img/navbar-Logo-I.svg' )
+	} else {
+		$('.logo').attr( 'src', 'img/navbar-Logo.svg' )
+		$('.navbar').css({ 'background-color': green })
+		$('.triangle').css({ 'border-color': greenTri })
+		$('.nav-links').css({ 'top': '60px' })
+	}
+
+	if(docWidth < 1280){
+		if (y > 625) {
+		$('.navbar').css({ 'background-color': blue })
+		$('.triangle').css({ 'border-color': blueTri })
+	} else {
+		$('.navbar').css({ 'background-color': green })
+		$('.triangle').css({ 'border-color': greenTri })
+	}
 }
 });
