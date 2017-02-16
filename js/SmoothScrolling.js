3f4ba34dc4db4ba39e35d421f235d4118fe726d6
Ethan Evans <graphicshark@gmail.com>
2017-02-16 13:56:39 -0700

    More JS fixes

--- js/SmoothScrolling.js
+++ js/SmoothScrolling.js
@@ -14,33 +14,6 @@ $(function() {
 	});
 });
 
-//var blue = {'background-color' : '#0F4556'};
-//var blueLogo = {src: 'img/navbar-Logo.svg'};
-//var blueTri = {'border-color': '#0F4556 transparent transparent transparent'};
-//
-//var green = {'background-color' :'#D2DBBD'};
-//var greenLogo = {src: 'img/navbar-Logo-I.svg'};
-//var greenTri = {'border-color': '#D2DBBD transparent transparent transparent'};
-//
-//$(document).scroll(function() {
-//	var y = $(this).scrollTop();
-//	var docWidth = $(document).width();
-//	switch (docWidth, y) {
-//		case (docWidth < 1000 && y > 150):
-//			$('.navbar').css(blue)
-//			$('.triangle').css(blueTri)
-//			$('.logo').attr(greenLogo)
-//			$('.nav-links').css({ 'top': '-5px' })
-//			break;
-//		default:
-//			$('.navbar').css(green)
-//			$('.triangle').css(greenTri)
-//			$('.logo').attr(blueLogo)
-//			$('.nav-links').css({ 'top': '60px' })
-//			break;
-//	}
-//});
-
 //Color change with page scroll
 $(document).scroll(function () {
 	var y = $(this).scrollTop();
